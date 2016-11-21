7b7497abeffff595b7a434e5ab173bc2e66c94ea
Javier Helguera <javier.helguera@alumnos.uva.es>
2016-11-21 14:37:24 +0100

    Trie Funcionando

---
+++ Trie.java
@@ -0,0 +1,58 @@
+
+
+import java.util.*;
+
+public class Trie {
+
+    HashMap<Integer, HashMap> root;
+
+    public Trie() {
+       root = new HashMap<Integer, HashMap>();
+    }
+
+
+    public void add(int [] s) {
+        HashMap<Integer, HashMap> nodo_actual = root;
+        for (int i = 0, n = s.length; i < n; i++) {
+            Integer c = s[i];
+            if (nodo_actual.containsKey(c))
+                nodo_actual = nodo_actual.get(c);
+            else {
+                nodo_actual.put(c, new HashMap<Integer, HashMap>());
+                nodo_actual = nodo_actual.get(c);
+            }
+        }
+    }
+
+    public boolean contiene(int[] s) {
+        HashMap<Integer, HashMap> nodo_actual = root;
+        for (int i = 0, n = s.length; i < n; i++) {
+            Integer c = s[i];
+            if (nodo_actual.containsKey(c))
+                nodo_actual = nodo_actual.get(c);
+            else {
+                return false;}
+        }
+        if (nodo_actual.containsKey(256)){
+        	nodo_actual = nodo_actual.get(256);
+        	for (int p = 0; p<65536; p++){
+        		if (nodo_actual.containsKey(p)){
+        			System.out.println(p);
+        			return true;
+        		}
+        	}
+            return true;}
+        else
+            return false;
+    }
+
+   public static void main(String[] args) {
+        Trie t = new Trie();
+        int evil_vector[] = {101, 118, 105, 108, 46,99, 111, 114, 112, 64, 109, 97, 100, 46, 111, 114, 103,256,65535};
+        int prueba[]={101, 118, 105, 108, 46,99, 111, 114, 112, 64, 109, 97, 100, 46, 111, 114, 103};
+        t.add(evil_vector);
+        //evil_vector[17]=1;
+        System.out.println(t.contiene(prueba));
+
+    }
+}
